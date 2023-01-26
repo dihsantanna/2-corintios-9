9c5267fb2f034097d34e7ce281729a68a20d2298
Diogo Sant'Anna <diogosantanna08@gmail.com>
2023-01-26 15:30:12 -0300

    refactor: improves tab navigation of the side menu and adds accessibility-oriented styling.

--- packages/renderer/src/components/Menu.tsx
+++ packages/renderer/src/components/Menu.tsx
@@ -29,20 +29,37 @@ export function Menu({setSelectedScreen, selectedScreen}: MenuProps) {
     <nav>
       <ul className="w-full text-center relative">
         {Object.entries(menuParams).map(([menuLabel, {label, items}]) => (
-        <li className="relative h-max" key={menuLabel}>
+          <li
+            title={`Abrir menu ${label}`}
+            className="relative h-max"
+            key={menuLabel}
+          >
           <button
-            className="relative bg-zinc-900 text-zinc-200 w-full h-11 py-2 border-b-2 border-b-zinc-700 hover:bg-teal-500 hover:text-zinc-900 rounded-b-sm z-50"
+              className={
+                `relative focus:outline-none ${menuOpened.indexOf(menuLabel as MenuLabelsType) === -1 ? 'focus:bg-teal-500 focus:text-zinc-900' : 'focus:bg-yellow-500 focus:text-zinc-900'} bg-zinc-900 text-zinc-200 w-full h-11 py-2 border-b-2 border-b-zinc-700 ${menuOpened.indexOf(menuLabel as MenuLabelsType) === -1 ? 'hover:bg-teal-500 hover:text-zinc-900' : 'hover:bg-yellow-500 hover:text-zinc-900'} rounded-b-sm z-50`
+            }
             onClick={() => toggleMenu(menuLabel as MenuLabelsType)}
-          >{label}</button>
-          <ul className={`${menuOpened.includes(menuLabel as MenuLabelsType) ? 'opacity-100' : 'absolute transform -translate-y-[1024px] opacity-0'} right-0 w-full z-10 text-zinc-300 transition-opacity duration-200 ease-linear`}>
+            >
+              {label}
+            </button>
+            <ul
+              className={`${menuOpened.includes(menuLabel as MenuLabelsType) ? 'opacity-100' : 'invisible absolute transform -translate-y-[1024px] opacity-0'} right-0 w-full z-10 text-zinc-300 transition-opacity duration-200 ease-linear`}>
               {items.map(({ id, label: subLabel }) => (
                 <li
-                  className={`py-2 ${selectedScreen === id
+                  title={subLabel}
+                  className={`${selectedScreen === id
                     ? 'bg-teal-500 text-zinc-900'
-                    : 'bg-zinc-400 text-zinc-200'} border-b-2 border-b-zinc-900 rounded-b-sm cursor-pointer hover:bg-teal-500 hover:text-zinc-900 text-sm`}
+                    : 'bg-zinc-300 text-zinc-900'} border-b-2 border-b-zinc-900 rounded-b-sm cursor-pointer hover:bg-teal-500 hover:text-zinc-900 text-sm`}
                   key={id}
-                  onClick={() => setSelectedScreen(id as Screens)}
-                >{subLabel}</li>
+                  >
+                  <button
+                    className="w-full h-full py-2 focus:outline-none focus:bg-teal-500 focus:text-zinc-900 disabled:cursor-not-allowed"
+                    onClick={() => setSelectedScreen(id as Screens)}
+                    disabled={selectedScreen === id}
+                  >
+                    {subLabel}
+                  </button>
+                </li>
             ))}
           </ul>
           </li>
@@ -50,7 +67,7 @@ export function Menu({setSelectedScreen, selectedScreen}: MenuProps) {
         <button
           title="Fechar todos os menus"
           className={
-            `absolute -bottom-9 w-full flex items-center justify-center h-9 ${
+            `focus:outline-teal-500 focus:outline-4 disabled:cursor-not-allowed absolute -bottom-9 w-full flex items-center justify-center h-9 ${
               menuOpened.length && 'hover:animate-pulse active:animate-bounce'
             } z-50`
           }
