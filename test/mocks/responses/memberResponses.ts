bbae979a9db2f721863e5009473ddc27782d08bd
Diogo Sant'Anna <diogosantanna08@gmail.com>
2023-03-15 12:25:47 -0300

    test: creates tests for the Member class.

---
+++ test/mocks/responses/memberResponses.ts
@@ -0,0 +1,61 @@
+export const createResolveResponse = {
+  err: null,
+  row: null,
+};
+
+export const createRejectResponse = {
+  err: new Error('Error'),
+  row: null,
+};
+
+export const findAllResolveResponse = {
+  err: null,
+  row: null,
+  rows: [
+    {
+      id: 'id1',
+      name: 'name1',
+      congregated: false,
+    },
+    {
+      id: 'id3',
+      name: 'name3',
+      congregated: true,
+    },
+    {
+      id: 'id2',
+      name: 'name2',
+      congregated: false,
+    },
+    {
+      id: 'id4',
+      name: 'name4',
+      congregated: false,
+    },
+  ],
+};
+
+export const findAllRejectResponse = {
+  err: new Error('Error'),
+  row: null,
+};
+
+export const updateResolveResponse = {
+  err: null,
+  row: null,
+};
+
+export const updateRejectResponse = {
+  err: new Error('Error'),
+  row: null,
+};
+
+export const deleteResolveResponse = {
+  err: null,
+  row: null,
+};
+
+export const deleteRejectResponse = {
+  err: new Error('Error'),
+  row: null,
+};
